d94ed94675565599094fe6ee3e67f79bddb30e19
Pamel-a <142637979+Pamel-a@users.noreply.github.com>
2024-01-06 15:38:44 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-const env = process.env.NODE_ENV || "dev"
+//const env = process.env.NODE_ENV || "dev"
+const env = Deno.env.get("NODE_ENV") || "dev";
 
 const configs = {
     dev: {
@@ -8,18 +9,22 @@ const configs = {
         port: 3001,
     },
 	production: {
-        mongodb_uri: process.env.MONGODB_URI,
-        port: process.env.PORT,
+        //mongodb_uri: process.env.MONGODB_URI,
+        //port: process.env.PORT,
+        mongodb_uri: Deno.env.get("MONGODB_URI"),
+        port: Deno.env.get("PORT"),
     }
 }
 
 Object.entries(configs).forEach(([env, arr]) => (arr.env = env))
 
 const config = configs[env]
-if (!config) throw new Error(`NODE_ENV \`${env}\` NOT CONFIGURED!`)
+if (!config) 
+    throw new Error(`NODE_ENV \`${env}\` NOT CONFIGURED!`)
 
 const badProperty = Object.entries(config).find(([key, value]) => value === undefined)
-if (badProperty) throw new Error(`NODE_ENV \`${env}\` KEY \`${badProperty[0]}\` is undefined! Please configure it!`)
+if (badProperty) 
+    throw new Error(`NODE_ENV \`${env}\` KEY \`${badProperty[0]}\` is undefined! Please configure it!`)
 
 //module.exports = config
 export default config
