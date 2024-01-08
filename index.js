8bab57eb91efadb631e85bff2a9ab9b5dc4d57f0
Pamel-a <142637979+Pamel-a@users.noreply.github.com>
2024-01-08 20:13:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,48 @@ import Bodyparser from "npm:koa-bodyparser";
 import Logger from "npm:koa-logger";
 import Cors from "npm:@koa/cors";
 import Mongo from "npm:koa-mongo";
-import config from "./config.js";
-import routes from "./routes.js";
+"use strict"
+
+/*
+const Koa = require("koa")
+const Bodyparser = require("koa-bodyparser")
+const Logger = require("koa-logger")
+const Cors = require("@koa/cors")
+const Mongo = require("koa-mongo")
+const config = require("./config")
+const routes = require("./routes")
+*/
+
+import Koa from "npm:koa";
+import Bodyparser from "npm:koa-bodyparser";
+import Logger from "npm:koa-logger";
+import Cors from "npm:@koa/cors";
+import Mongo from "npm:koa-mongo";
+import {config} from "./config.js";
+import {routes} from "./routes.js";
+
+const app = new Koa()
+
+app.proxy = true
+
+app.use(
+    Mongo({
+        url: config.mongodb_uri,
+        max: 100,
+        min: 1
+    })
+)
+
+app.use(Logger())
+app.use(Cors({ origin: "*", credentials: false }))
+app.use(Bodyparser({ jsonLimit: "15mb" }))
+
+routes.forEach(route => app.use(route.routes()))
+
+app.listen(config.port, () => {
+    console.log(`🚀 API running on ${config.env} port ${config.port}`)
+})
+
 
 const app = new Koa()
 
