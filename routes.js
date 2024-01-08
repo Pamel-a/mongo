2b8b650348a6a2af6ceff6d45da0d4f1454fe88f
Pamel-a <142637979+Pamel-a@users.noreply.github.com>
2024-01-08 20:03:28 -0300

    Add files via upload

--- routes.js
+++ routes.js
@@ -1,27 +1,5 @@
 "use strict"
 
-/*
-const Router = require("koa-router")
-const bind = require("koa-clean")
-
-const UserController   = require ("user-controller")
-const VarController   = require ("var-controller")
-
-const users = new Router({ prefix: "/users" })
-	.post ("/slot", bind (UserController.slot))
-
-const vars = new Router({ prefix: "/vars" })
-	.post  ("/get",  bind (VarController.get))
-	.post ("/set",  bind (VarController.set))
-
-const root = new Router({ prefix: "/api" })
-	.use  (users.routes())
-	.use  (vars.routes())
-
-module.exports = root
-*/
-
-
 // Import Koa Router and middleware
 import Router from 'npm:@koa/router';
 import bind from 'npm:koa-clean'; 
@@ -29,8 +7,8 @@ import bind from 'npm:koa-clean';
 import VarController from './var-controller.js';
 
 const vars = new Router({ prefix: "/vars" })
-  .post("/get", bind(VarController.get))
-  .post("/set", bind(VarController.set));
+  .post("/get", bind(VarController._get))
+  .post("/set", bind(VarController._set));
 
 const root = new Router({ prefix: "/api" })
   .use(vars.routes());
