bff24894f4faf1ca7ff5ca6cf8e1491ebbd25827
Pamel-a <142637979+Pamel-a@users.noreply.github.com>
2024-01-08 20:09:24 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -4,11 +4,11 @@
 import Router from 'npm:@koa/router';
 import bind from 'npm:koa-clean'; 
 
-import {_get, _set} from './var-controller.js';
+import {get, set} from './var-controller.js';
 
 const vars = new Router({ prefix: "/vars" })
-  .post("/get", bind(_get))
-  .post("/set", bind(_set));
+  .post("/get", bind(get))
+  .post("/set", bind(set));
 
 const root = new Router({ prefix: "/api" })
   .use(vars.routes());
