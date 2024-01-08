bd0ae40bffef05f048f73ae1fc530081681ba584
Pamel-a <142637979+Pamel-a@users.noreply.github.com>
2024-01-08 19:54:35 -0300

    Update var-controller.js

--- var-controller.js
+++ var-controller.js
@@ -1,11 +1,11 @@
 "use strict"
 
 // const Vars  = require ("vars")
-import Vars from "./vars.js"
+import {get, set} from "./vars.js"
 
 export const get = async ({ db }, { key, sub_key }) => 
 {
-    const variable = await Vars.get (db, key)
+    const variable = await get (db, key)
     console.log("variable: ", variable)
 
     if (variable)
@@ -16,7 +16,7 @@ export const get = async ({ db }, { key, sub_key }) =>
 
 export const set = async ({ db }, { key, value }) => 
 {
-    Vars.set (db, key, value)
+    set (db, key, value)
     return "ok"
 }
 
