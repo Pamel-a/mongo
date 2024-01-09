e4957abd7a0bcf422a07f79328e0abff78781cf4
Pamel-a <142637979+Pamel-a@users.noreply.github.com>
2024-01-09 01:34:36 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -10,11 +10,7 @@ const get = async ({ db }, { key }) => {
     console.log("get: ", key)
     const variable = await _get (db, key)
     console.log("variable: ", variable)
-
-    if (variable)
-        return variable
-    else
-        return [ 404, "not found" ]    
+    return variable ? variable : [404, "not found"];
 }
 
 const vars = new Router({ prefix: "/vars" })
